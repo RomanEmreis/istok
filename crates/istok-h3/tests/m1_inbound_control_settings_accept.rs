f8f4adb331275b49209fcce2ef41e46d381f0e5d
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-02-23 17:30:24 +0400

    h3: accept inbound control settings frame

---
+++ crates/istok-h3/tests/m1_inbound_control_settings_accept.rs
@@ -0,0 +1,41 @@
+extern crate alloc;
+
+use istok_core::codec::{h3_frame, varint};
+use istok_core::h3::consts;
+use istok_h3::H3Engine;
+use istok_h3::mock::{MockHarness, ScriptStep};
+use istok_transport::{StreamId, StreamKind};
+
+#[test]
+fn inbound_control_stream_accepts_empty_settings_without_closing() {
+    let engine = H3Engine::new();
+    let mut h = MockHarness::new(engine);
+
+    let peer_uni_id = StreamId(3);
+
+    let mut buf = [0u8; 16];
+    let stream_ty_len =
+        varint::encode(consts::STREAM_TYPE_CONTROL, &mut buf).expect("stream type encodes");
+    let frame_len = h3_frame::encode_frame_header(
+        h3_frame::FrameHeader {
+            ty: consts::FRAME_TYPE_SETTINGS,
+            len: 0,
+        },
+        &mut buf[stream_ty_len..],
+    )
+    .expect("frame header encodes");
+
+    h.run_script(&[
+        ScriptStep::InQuicOpen {
+            id: peer_uni_id,
+            kind: StreamKind::Uni,
+        },
+        ScriptStep::ExpectNone,
+        ScriptStep::InQuicData {
+            id: peer_uni_id,
+            data: alloc::vec::Vec::from(&buf[..stream_ty_len + frame_len]),
+            fin: false,
+        },
+        ScriptStep::ExpectNone,
+    ]);
+}
